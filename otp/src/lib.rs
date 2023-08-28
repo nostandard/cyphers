da2271543b6011240aa15dbd77ad581bbc27e914
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-08-28 21:35:59 +0000

    Implement basic encryption algorithm for the OTP

--- otp/src/lib.rs
+++ otp/src/lib.rs
@@ -12,6 +12,21 @@ pub fn generate_key(len: usize) -> Vec<u8> {
     key
 }
 
+pub fn encrypt(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
+    if plaintext.len() != key.len() {
+        panic!("Key and plaintext lengths do not match!");
+    }
+
+    let mut ciphertext: Vec<u8> = vec![];
+
+    for i in 0..plaintext.len() {
+        let encrypted_bit = plaintext[i] ^ key[i];
+        ciphertext.push(encrypted_bit);
+    }
+
+    ciphertext
+}
+
 fn generate_random_bit() -> u8 {
     let mut key: Vec<u8> = vec![];
     OsRng.fill_bytes(&mut key);
@@ -28,5 +43,16 @@ mod tests {
         let key = generate_key(32);
         println!("Key: {:?}", &key);
         assert_eq!(key.len(), 32);
+    }
+
+    #[test]
+    fn test_encrypt() {
+        let plaintext = "Hello";
+        let key = generate_key(plaintext.len());
+        let ciphertext = encrypt(plaintext.as_bytes(), &key);
+
+        assert_eq!(plaintext.len(), key.len());
+        assert_eq!(plaintext.len(), ciphertext.len());
+        assert_eq!(key.len(), ciphertext.len());
     }
 }
