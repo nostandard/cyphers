f93f6cb222ef3374ad609255e6160657eaadfb4f
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-08-28 22:13:25 +0000

    Refactor for a more compact implementation of the OTP

--- otp/src/lib.rs
+++ otp/src/lib.rs
@@ -3,69 +3,50 @@
 use rand::rngs::OsRng;
 use rand::RngCore;
 
+/// Generates a key of the specified length using true randomness.
+///
+/// # Arguments
+///
+/// * `len` - The desired length of the key.
 pub fn generate_key(len: usize) -> Vec<u8> {
-    // Create a vec of truly random bits of the given length.
-    let mut key = Vec::with_capacity(len);
-    for _ in 0..len {
-        key.push(generate_random_bit())
-    }
+    let mut key = vec![0u8; len];
+    OsRng.fill_bytes(&mut key);
     key
 }
 
+/// Encrypts the given plaintext using the specified key.
+///
+/// # Arguments
+///
+/// * `plaintext` - The data to be encrypted.
+/// * `key` - The key to use for encryption.
 pub fn encrypt(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
-    if plaintext.len() != key.len() {
-        panic!("Key and plaintext lengths do not match!");
-    }
-
-    let mut ciphertext: Vec<u8> = vec![];
-
-    for i in 0..plaintext.len() {
-        // Encrypt the plaintext using the key
-        let encrypted_bit = plaintext[i] ^ key[i];
-        ciphertext.push(encrypted_bit);
-    }
-
-    ciphertext
+    encipher(plaintext, key)
 }
 
+/// Decrypts the given ciphertext using the specified key.
+///
+/// # Arguments
+///
+/// * `ciphertext` - The data to be decrypted.
+/// * `key` - The key to use for decryption.
 pub fn decrypt(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
-    if ciphertext.len() != key.len() {
-        panic!("Key and ciphertext lengths do not match!");
-    }
-
-    let mut plaintext: Vec<u8> = vec![];
-
-    for i in 0..ciphertext.len() {
-        // Decrypt the ciphertext using the key
-        let decrypted_bit = ciphertext[i] ^ key[i];
-        plaintext.push(decrypted_bit);
-    }
-
-    plaintext
+    encipher(ciphertext, key)
 }
 
-// Generic implementation for both encryption and decryption
+/// Encrypts or decrypts the data using the given key.
+/// This generic function can be used for both encryption and decryption since the OTP is symmetrical.
+///
+/// # Arguments
+///
+/// * `data` - The plaintext (for encryption) or ciphertext (for decryption).
+/// * `key` - The key to use for encryption/decryption.
 pub fn encipher(data: &[u8], key: &[u8]) -> Vec<u8> {
     if data.len() != key.len() {
         panic!("The lengths of the data and the key do not match!");
     }
 
-    let mut enciphered_data: Vec<u8> = vec![];
-
-    for i in 0..data.len() {
-        // Encrypt/decrypt the data using the key
-        let enciphered_bit = data[i] ^ key[i];
-        enciphered_data.push(enciphered_bit);
-    }
-
-    enciphered_data
-}
-
-fn generate_random_bit() -> u8 {
-    let mut key: Vec<u8> = vec![];
-    OsRng.fill_bytes(&mut key);
-    let random_u64 = OsRng.next_u64();
-    (random_u64 & 1) as u8
+    data.iter().zip(key.iter()).map(|(a, b)| a ^ b).collect()
 }
 
 #[cfg(test)]
