6fbde68862aa88a5ab6806ce6d8fbface067cf66
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-08-29 22:57:06 +0000

    The Caesar cipher works!

--- caesar/src/lib.rs
+++ caesar/src/lib.rs
@@ -35,3 +35,42 @@ fn process_char(c: char, mode: &Mode, key: i32, offset: char) -> char {
     };
     (encrypted_char_pos + offset as i32) as u8 as char
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_caesar_encryption() {
+        let plaintext = "Hello";
+        let encrypted = encrypt(plaintext, 3);
+        assert_eq!(encrypted, "Khoor");
+    }
+
+    #[test]
+    fn test_caesar_decryption() {
+        let ciphertext = "Khoor";
+        let decrypted = decrypt(ciphertext, 3);
+        assert_eq!(decrypted, "Hello");
+    }
+
+    #[test]
+    fn test_caesar_encryption_and_decryption() {
+        let plaintext = "HelloWorld";
+        let encrypted = encrypt(plaintext, 3);
+        assert_eq!(encrypted, "KhoorZruog");
+
+        let decrypted = decrypt(&encrypted, 3);
+        assert_eq!(decrypted, plaintext);
+    }
+
+    #[test]
+    fn test_caesar_encryption_and_decryption_non_alpha_chars() {
+        let plaintext = "Hello, World!";
+        let encrypted = encrypt(plaintext, 3);
+        assert_eq!(encrypted, "Khoor, Zruog!");
+
+        let decrypted = decrypt(&encrypted, 3);
+        assert_eq!(decrypted, plaintext);
+    }
+}
