7359ec36920e01d3ab984cfc9e9918600b4350ea
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-09-04 21:50:32 +0000

    Implement the `ROT13` cipher

--- rot13/src/lib.rs
+++ rot13/src/lib.rs
@@ -1,1 +1,20 @@
 //! An implementation of the ROT13 cipher
+
+pub fn rot13(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphabetic() {
+                let base = if c.is_ascii_lowercase() {
+                    'a' as u8
+                } else {
+                    'A' as u8
+                };
+                let offset = (c as u8 - base + 13) % 26;
+                (base + offset) as char
+            } else {
+                c
+            }
+        })
+        .collect()
+}
