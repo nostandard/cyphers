28adae4d1c16059ff0925ec4e35214580ebd42a5
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-09-04 22:06:03 +0000

    ROT13 encryption and decryption work!

--- rot13/src/lib.rs
+++ rot13/src/lib.rs
@@ -8,12 +8,16 @@ const ALPHABET_SIZE: u8 = 26;
 /// The ROT13 transformation is performed on each alphabetic character in the input string.
 /// Non-alphabetic characters are left unchanged.
 ///
+/// It is worth noting that ROT13 is its own inverse; applying it twice will get you back the original input.
+/// Thus, this function is used both to encrypt plaintext to ciphertext, and to decrypt ciphertext to plaintext.
+///
 /// # Examples
 ///
 /// ```
 /// use rot13::rot13;
 ///
 /// assert_eq!(rot13("Hello, World!"), "Uryyb, Jbeyq!");
+/// assert_eq!(rot13("Uryyb, Jbeyq!"), "Hello, World!");
 /// ```
 pub fn rot13(input: &str) -> String {
     input
@@ -38,4 +42,41 @@ pub fn rot13(input: &str) -> String {
         })
         // Collect the modified characters into a new string and return it.
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rot13_with_lowercase() {
+        assert_eq!(
+            rot13("abcdefghijklmnopqrstuvwxyz"),
+            "nopqrstuvwxyzabcdefghijklm"
+        );
+    }
+
+    #[test]
+    fn test_rot13_with_uppercase() {
+        assert_eq!(
+            rot13("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+            "NOPQRSTUVWXYZABCDEFGHIJKLM"
+        );
+    }
+
+    #[test]
+    fn test_rot13_with_mixed_case() {
+        assert_eq!(rot13("Hello, World!"), "Uryyb, Jbeyq!");
+    }
+
+    #[test]
+    fn test_rot13_with_non_alphabetic() {
+        assert_eq!(rot13("12345!@#$%^"), "12345!@#$%^");
+    }
+
+    #[test]
+    fn test_rot13_idempotent() {
+        let input = "ROT13 is Idempotent!";
+        assert_eq!(rot13(&rot13(input)), input);
+    }
 }
