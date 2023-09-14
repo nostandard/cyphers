728bffcd3b11e11e005a612509c5a7f274ffaa3c
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-09-14 14:48:32 +0000

    Polybius-Square cipher: add documentation comments

--- src/polybius_square.rs
+++ src/polybius_square.rs
@@ -1,5 +1,6 @@
-//! An implementation of Polybius Square cipher
+//! An implementation of the Polybius Square cipher.
 
+/// The grid used for encryption and decryption.
 const GRID: [[char; 5]; 5] = [
     ['A', 'B', 'C', 'D', 'E'],
     ['F', 'G', 'H', 'I', 'K'],
@@ -8,6 +9,15 @@ const GRID: [[char; 5]; 5] = [
     ['V', 'W', 'X', 'Y', 'Z'],
 ];
 
+/// Encrypts a plaintext message using the Polybius Square cipher.
+///
+/// # Arguments
+///
+/// * `plaintext` - The message to encrypt.
+///
+/// # Returns
+///
+/// * A string containing the ciphertext.
 pub fn encrypt(plaintext: &str) -> String {
     let cleaned_input = clean_input(plaintext);
     cleaned_input
@@ -16,6 +26,15 @@ pub fn encrypt(plaintext: &str) -> String {
         .collect()
 }
 
+/// Decrypts a ciphertext message using the Polybius Square cipher.
+///
+/// # Arguments
+///
+/// * `ciphertext` - The message to decrypt.
+///
+/// # Returns
+///
+/// * An option containing a string with the decrypted message if successful, or `None` if the input is invalid.
 pub fn decrypt(ciphertext: &str) -> Option<String> {
     if ciphertext.len() % 2 != 0 {
         return None;
@@ -35,6 +54,15 @@ pub fn decrypt(ciphertext: &str) -> Option<String> {
     Some(plaintext)
 }
 
+/// Cleans the input string by converting to uppercase and filtering out invalid characters.
+///
+/// # Arguments
+///
+/// * `input` - The string to clean.
+///
+/// # Returns
+///
+/// * A string containing only valid characters in uppercase.
 fn clean_input(input: &str) -> String {
     input
         .to_uppercase()
@@ -43,6 +71,15 @@ fn clean_input(input: &str) -> String {
         .collect()
 }
 
+/// Finds the coordinates of a character in the grid.
+///
+/// # Arguments
+///
+/// * `ch` - The character to find.
+///
+/// # Returns
+///
+/// * An option containing a tuple with the coordinates (row, col) if the character is found, or `None` otherwise.
 fn find_coordinates(ch: char) -> Option<(usize, usize)> {
     for (i, row) in GRID.iter().enumerate() {
         for (j, &cell) in row.iter().enumerate() {
@@ -54,6 +91,15 @@ fn find_coordinates(ch: char) -> Option<(usize, usize)> {
     None
 }
 
+/// Finds the character in the grid at the given coordinates.
+///
+/// # Arguments
+///
+/// * `coordinates` - A tuple containing the row and column indices.
+///
+/// # Returns
+///
+/// * An option containing the character if the coordinates are valid, or `None` otherwise.
 fn find_char(coordinates: (usize, usize)) -> Option<char> {
     if coordinates.0 < 5 && coordinates.1 < 5 {
         Some(GRID[coordinates.0][coordinates.1])
