e232c314db5f94c6b917f8841fdd1b10268ff29b
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-09-26 19:01:20 +0000

    WIP: `porta` cipher implementation

--- src/porta.rs
+++ src/porta.rs
@@ -50,12 +50,11 @@ fn encipher(text: &str, key: &str, mode: Mode) -> String {
             match mode {
                 Mode::Encrypt => index_to_char((text_idx + key_idx) % 13, true),
                 Mode::Decrypt => {
-                    let cipher_idx = if text_idx < 13 {
-                        text_idx
+                    if text_idx < 13 {
+                        index_to_char((13 + text_idx - key_idx) % 13, false)
                     } else {
-                        text_idx - 13
-                    };
-                    index_to_char((cipher_idx + 26 - key_idx) % 13, false)
+                        index_to_char((26 + text_idx - key_idx) % 13, true)
+                    }
                 }
             }
         })
@@ -83,3 +82,35 @@ fn index_to_char(index: usize, is_second_half: bool) -> char {
     let adjusted_index = if is_second_half { index + 13 } else { index };
     char::from_u32((adjusted_index + 'A' as usize) as u32).unwrap()
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_porta_encrypt() {
+//         let plaintext = "HELLO";
+//         let key = "KEY";
+//         let encrypted = encrypt(plaintext, key);
+//         assert_eq!(encrypted, "RYXXO");
+//     }
+
+//     #[test]
+//     fn test_porta_decrypt() {
+//         let ciphertext = "RYXXO";
+//         let key = "KEY";
+//         let decrypted = decrypt(ciphertext, key);
+//         assert_eq!(decrypted, "HELLO");
+//     }
+
+//     #[test]
+//     fn test_porta_non_alphabetic_chars() {
+//         let plaintext = "HELLO123!";
+//         let key = "KEY";
+//         let encrypted = encrypt(plaintext, key);
+//         assert_eq!(encrypted, "RYXXO");
+
+//         let decrypted = decrypt("RYXXO123!", key);
+//         assert_eq!(decrypted, "HELLO");
+//     }
+// }
